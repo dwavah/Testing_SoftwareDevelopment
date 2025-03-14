47aec4008ffe8d1ed3ce663a18f9e658c6756677
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-14 11:57:30 +0300

    updating Django/views.py

---
+++ Django/backend/users/views.py
@@ -0,0 +1,16 @@
+# users/views.py
+
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key})
